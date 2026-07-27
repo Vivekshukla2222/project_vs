b9648eef528da9c5105833db16b23fe4627af217
Vivekshukla2222 <168322702+Vivekshukla2222@users.noreply.github.com>
2026-07-27 17:29:26 +0000

    add scholl name

--- new.py
+++ new.py
@@ -1,7 +1,9 @@
 class Student:
+    scholl_name='DPS'
     def __init__(self,name,age):
         self.name=name
         self.age=age
 
 s1=Student("Amit",23)
+print(s1.scholl_name)
 print(s1.name,s1.age)
